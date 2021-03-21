24b1a6764715353bcc2f66a73b65c3af02331a4d
ilyasiluyanov <isiluyanov8@gmail.com>
2021-03-21 14:28:52 +0300

    url mappings was changed

--- YandexSweetsProject/urls.py
+++ YandexSweetsProject/urls.py
@@ -1,6 +1,5 @@
 from django.urls import path, include
 
 urlpatterns = [
-    path('', include('YandexSweets.urls')),
-    path('/', include('YandexSweets.urls'))
+    path('/', include('YandexSweets.urls')),
 ]
