81d309a12752e55f3aa396c380ed6e9d5fa408ef
ilyasiluyanov <isiluyanov8@gmail.com>
2021-03-24 21:59:16 +0300

    config for gunicorn was added

---
+++ YandexSweetsProject/gunicorn_config.py
@@ -0,0 +1,12 @@
+# path to gunicorn
+command = '/home/entrant/YandexSweets/env/bin/gunicorn'
+# path to directory with config and settings.py
+pythonpath = '/home/entrant/YandexSweets/YandexSweetsProject'
+# host and port on which gunicorn will work
+bind = '127.0.0.1:8001'
+# workers = cores*2+1
+workers = 9
+user = 'entrant'
+limit_request_fields = 32000
+limit_request_fields_size = 0
+raw_env = 'DJANGO_SETTINGS_MODULE=YandexSweetsProject.settings'
---
+++ bin/start_gunicorn.sh
@@ -0,0 +1,4 @@
+#!/bin/bash
+#activate virtual environment
+source /home/entrant/YandexSweets/yandex_env/bin/activate
+exec gunicorn -c "/home/entrant/YandexSweets/YandexSweetsProject/gunicorn_config.py" YandexSweetsProject.wsgi
