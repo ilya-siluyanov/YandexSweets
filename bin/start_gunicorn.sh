b660fb1511660f555b79809cc64110e83e35a540
ilyasiluyanov <isiluyanov8@gmail.com>
2021-03-24 21:33:02 +0300

    all production stuff was removed

--- bin/start_gunicorn.sh
+++
@@ -1,5 +0,0 @@
-#!/bin/bash
-source /home/entrant/YandexSweets/yandex_env/bin/activate #absolute path to virtual environment
-export SECRET_KEY_PATH=/usr/bin/secret_key.txt            #absolute path to text file with only secret key of project
-#absolute path to gunicorn_config.py
-exec gunicorn -c "/home/entrant/YandexSweets/YandexSweetsProject/gunicorn_config.py" YandexSweetsProject.wsgi
