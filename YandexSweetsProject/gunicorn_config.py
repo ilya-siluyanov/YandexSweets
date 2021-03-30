6178f5d72ee4b5b7506e1707496c36da3e13a9c3
ilyasiluyanov <isiluyanov8@gmail.com>
2021-03-30 15:52:33 +0300

    gunicorn_config.py was updated

--- YandexSweetsProject/gunicorn_config.py
+++ YandexSweetsProject/gunicorn_config.py
@@ -9,4 +9,4 @@ workers = 9
 user = 'entrant'
 limit_request_fields = 32000
 limit_request_fields_size = 0
-raw_env = 'DJANGO_SETTINGS_MODULE=YandexSweetsProject.settings'
+raw_env = 'DJANGO_SETTINGS_MODULE=YandexSweetsProject.production_settings'
