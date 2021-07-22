9467044d5935ba5b55bd40f881b608f6a6771b62
Ilya Siluyanov <isiluyanov8@gmail.com>
2021-07-22 11:11:43 +0300

    Dockerised project without gunicorn

--- Dockerfile
+++
@@ -1,5 +0,0 @@
-FROM python:latest
-
-COPY . /app
-RUN pip install -r requirements.txt
-CMD python /app/manage.py runserver 127.0.0.1:8000
---
+++ server/YandexSweetsProject/gunicorn_config.py
@@ -0,0 +1,19 @@
+import os
+
+
+# path to gunicorn
+command = '/usr/local/bin/gunicorn'
+# path to directory with config and settings.py
+pythonpath = '/app/YandexSweetsProject'
+# host and port on which gunicorn will work
+bind = '0.0.0.0:8000'
+# workers = cores*2+1
+workers = 9
+user = 'root'
+limit_request_fields = 32000
+limit_request_fields_size = 0
+raw_env = 'DJANGO_SETTINGS_MODULE=YandexSweetsProject.settings'
+accesslog = '/app/logs/access.log'
+errorlog = '/app/logs/error.log'
+capture_output = True
+loglevel = 'debug'
